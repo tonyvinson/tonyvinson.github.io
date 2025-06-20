6ecd7375bb2a78a15d44c730b138426292531163
tonyvinson <tonyvinson@gmail.com>
2025-06-20 16:03:36 -0400

    Modernize step 1

---
+++ assets/js/modern-enhancements.js
@@ -0,0 +1,42 @@
+// Modern JavaScript Enhancements
+(function($) {
+  "use strict";
+
+  // Navbar Shrink Effect
+  $(window).scroll(function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  });
+
+  // Enhanced Form Handling
+  $("#contactForm").on("submit", function(e) {
+    var $button = $("#sendMessageButton");
+    var originalText = $button.html();
+    
+    // Add loading state
+    $button.addClass("btn-loading").prop("disabled", true);
+    
+    // Reset after form submission
+    setTimeout(function() {
+      $button.removeClass("btn-loading").html(originalText).prop("disabled", false);
+    }, 3000);
+  });
+
+  // Smooth Scroll Enhancement for all anchors
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+})(jQuery);
